046620583fc4f50717ddd5031945c0f03ec8a372
Robin <robin.sowell@ellislab.com>
2021-01-12 11:32:32 -0500

    Initial commit for EEv6

---
+++ user/language/italian/block_and_allow_lang.php
@@ -0,0 +1,143 @@
+<?php
+$lang = array(
+
+
+'block_and_allow_module_name' => 
+'Blocca e consenti',
+
+'block_and_allow_module_description' => 
+'Blocca/consenti indirizzi IP, URL e User-Agents',
+
+'add_htaccess_file' => 
+'Aggiungere al file .htaccess?',
+
+'add_htaccess_file_desc' => 
+'Percorso completo del file <code>.htaccess</code>.',
+
+'btn_save_list' => 
+'Salva elenco',
+
+'btn_download_blockedlist' => 
+'Scarica la lista dell\'elenco dei blocchi di EE',
+
+'btn_download_allowedlist' => 
+'Scarica la lista dell\'elenco consentito di EE',
+
+'btn_save_settings' => 
+'Salva impostazioni',
+
+'settings' => 
+'Impostazioni',
+
+'lists' => 
+'Elenco',
+
+'allowedlist' => 
+'Elenco consentito',
+
+'allowedlist_desc' => 
+'Gli elenchi consentiti <b>permettono</b> l\'accesso al sito web tramite un indirizzo <abbr title="Internet Protocol">IP </abbr> specifico, user agents e <abbr title="Unified Resource Locator">URL</abbr>.',
+
+'blockedlist' => 
+'Elenco bloccato',
+
+'blockedlist_desc' => 
+'Gli elenchi bloccati <b>non consentono</b> l\'accesso al sito web tramite un indirizzo <abbr title="Internet Protocol">IP</abbr> specifico, user agents e <abbr title="Unified Resource Locator">URL</abbr>.',
+
+'ip_address' => 
+'Indirizzi <abbr title="Internet Protocol">IP</abbr>',
+
+'ip_address_desc' => 
+'Inserire ciascun indirizzo <abbr title="Internet Protocol">IP</abbr> su una linea separata.<br>Puoi utilizzare <code>*</code> come carattere jolly, ad es. 123.456. <code>*</code>',
+
+'user_agent' => 
+'User Agent',
+
+'user_agent_desc' => 
+'Inserire ogni user agent su una linea separata.',
+
+'url' => 
+'<abbr title="Unified Resource Locator">URL</abbr>',
+
+'url_desc' => 
+'Inserire ogni <abbr title="Unified Resource Locator">URL</abbr> su una linea separata.',
+
+'htaccess_path_removed' => 
+'Percorso all\'.htaccess rimosso',
+
+'htaccess_path_removed_desc' => 
+'Il percorso al file .htaccess è stato rimosso dalla configurazione',
+
+'htaccess_written_successfully' => 
+'File .htaccess aggiornato',
+
+'htaccess_written_successfully_desc' => 
+'Il file .htaccess è stato scritto correttamente.',
+
+'invalid_htaccess_path' => 
+'Percorso o autorizzazioni non validi per il file .htaccess',
+
+'lists_updated' => 
+'Elenchi aggiornati',
+
+'lists_updated_desc' => 
+'Gli elenchi bloccati e consentiti sono stati aggiornati correttamente.',
+
+'blockedlist_downloaded' => 
+'L\'elenco bloccato è stato aggiornato con il download da ExpressionEngine.',
+
+'allowedlist_downloaded' => 
+'L\'elenco consentito è stato aggiornato con il download da ExpressionEngine.',
+
+'ee_allowedlist' => 
+'Scarica la lista degli elenchi consentiti di ExpressionEngine',
+
+'ref_allowedlist_irretrievable' => 
+'Errore: la nuova lista degli elenchi consentiti era irrecuperabile.',
+
+'ref_view_allowedlist' => 
+'Visualizza la lista degli elenchi consentiti',
+
+'ref_no_allowedlist' => 
+'Al momento non è elencato nulla da consentire',
+
+'ref_allowedlisted' => 
+'Elencato per consentire',
+
+'ref_no_allowedlist_table' => 
+'La tabella del database degli elenchi consentiti non esiste',
+
+'ref_type' => 
+'Tipo di elemento',
+
+'ee_blockedlist' => 
+'Scarica la lista dei blocchi di ExpressionEngine',
+
+'ref_blockedlist_irretrievable' => 
+'Errore: il nuovo elenco di elementi bloccati era irreversibile.',
+
+'ref_view_blockedlist' => 
+'Visualizza elenco dei blocchi',
+
+'ref_no_blockedlist' => 
+'Al momento non è presente alcun elenco da bloccare',
+
+'ref_ip' => 
+'Indirizzo IP',
+
+'ref_user_agent' => 
+'User Agent',
+
+'ref_url' => 
+'URL',
+
+'ref_blockedlisted' => 
+'Elencato per bloccare',
+
+'ref_no_blockedlist_table' => 
+'La tabella del database da bloccare non esiste',
+
+''=>''
+);
+
+// End of File
---
+++ user/language/italian/jump_menu_lang.php
@@ -0,0 +1,284 @@
+<?php
+$lang = array(
+
+
+'shortcut' => 
+'Scorciatoia',
+
+'addon' => 
+'Compnente aggiuntivo',
+
+'jump_home' => 
+'Vai alla <b>home del pannello</b> ',
+
+'jump_viewEntriesIn' => 
+'Visualizza <b>Voci</b> in <i>[channel]</i>',
+
+'jump_createEntryIn' => 
+'Crea <b>voci</b> in <i>[channel]</i>',
+
+'jump_editEntry' => 
+'Modifica <b>voci</b> intitolate <i>[channel]</i>',
+
+'jump_createMember' => 
+'Crea <b>utente</b>',
+
+'jump_createMemberRole' => 
+'Crea <b>ruolo utente</b>',
+
+'jump_createMemberField' => 
+'Crea <b>campo utente</b>',
+
+'jump_createCategoryIn' => 
+'Cra <b>categoria</b> in <i>[category group]</i>',
+
+'jump_editCategory' => 
+'Modifica <b>categoria</b> intitolata <i>[category]</i>',
+
+'jump_viewFiles' => 
+'Visualizza <b>tutti i files</b>',
+
+'jump_viewFilesIn' => 
+'Visualizza <b>files</b> in <i>[upload directory]</i>',
+
+'jump_createUploadDirectory' => 
+'Crea <b>cartella di upload</b>',
+
+'jump_editUploadDirectory' => 
+'Modifica <b>cartella di upload</b>',
+
+'jump_syncUploadDirectory' => 
+'Sincroniza <b>cartella di upload</b>',
+
+'jump_membersBanSettings' => 
+'Gestisci le <b>impostazioni di ban</b>',
+
+'jump_viewMembers' => 
+'Visualizza <b>utenti</b>',
+
+'jump_viewMembersIn' => 
+'Visualizza <b>utenti</b> in <i>[member role]</i>',
+
+'jump_editMember' => 
+'Modifica <b>utente</b> intitolato <i>[name]</i>',
+
+'jump_editMemberRole' => 
+'Modifica <b>ruolo utente</b> intitolato <i>[name]</i>',
+
+'jump_viewAddons' => 
+'Visualizza <b>componenti aggiuntivi</b>',
+
+'jump_viewChannels' => 
+'Visualizza <b>canali</b>',
+
+'jump_viewLayouts' => 
+'Visualizza <b>pubblica layout</b> per <i>[channel]</i>',
+
+'jump_viewChannelFields' => 
+'Visualizza <b>campi canale</b>',
+
+'jump_createChannel' => 
+'Crea <b>canale</b>',
+
+'jump_editChannel' => 
+'Modifica <b>canale</b> intitolato <i>[channel]</i>',
+
+'jump_createChannelField' => 
+'Crea <b>campo canale</b>',
+
+'jump_editChannelField' => 
+'Modifca <b>campo canale</b> intitolato <i>[field]</i>',
+
+'jump_viewTemplates' => 
+'Visualizza <b>template</b>',
+
+'jump_viewTemplatePartials' => 
+'Visualizza <b>parziale template</b>',
+
+'jump_viewTemplateVariables' => 
+'Visualizza <b>variabili template</b>',
+
+'jump_templateRoutes' => 
+'Gestisce <b>percorsi template</b>',
+
+'jump_viewTemplatesIn' => 
+'Visualizza <b>template</b> in <i>[group]</i>',
+
+'jump_createTemplateGroup' => 
+'Crea <b>gruppo template</b>',
+
+'jump_createTemplateIn' => 
+'Crea <b>template</b> in <i>[group]</i>',
+
+'jump_editTemplate' => 
+'Modifca <b>template</b> intitolato <i>[template]</i>',
+
+'jump_editTemplateGroup' => 
+'Modifica <b>gruppo tempolate</b> intitolato <i>[group]</i>',
+
+'jump_systemTemplatesMessages' => 
+'Visualizza <b>template dei messaggi di sistema</b>',
+
+'jump_systemTemplatesEmail' => 
+'Visualizza <b>template email</b>',
+
+'jump_systemTemplatesMembers' => 
+'Visualizza <b>template del profilo utente</b>',
+
+'jump_systemSettingsGeneral' => 
+'Modifica impostazioni <b>generali</b>',
+
+'jump_systemSettingsUrls' => 
+'Modifica impostazioni generali &raquo; Impostazioni <b>URL e percorso</b>',
+
+'jump_systemSettingsEmail' => 
+'Modifica impostazioni generali &raquo; Impostazioni <b>posta in uscita</b>',
+
+'jump_systemSettingsDebugging' => 
+'Modifica impostazioni generali &raquo; Impostazioni di <b>debug</b>',
+
+'jump_systemSettingsContentDesign' => 
+'Modifica le impostazioni di <b>contenuto e design</b>',
+
+'jump_systemSettingsComments' => 
+'Modifica contenuto e design &raquo; Impostazioni <b>commento</b>',
+
+'jump_systemSettingsButtons' => 
+'Modifica contenuto e design &raquo; Impostazioni dei <b>pulsanti HTML</b>',
+
+'jump_systemSettingsTemplate' => 
+'Modifica contenuto e design &raquo; Impostazioni <b>template</b>',
+
+'jump_systemSettingsHitTracking' => 
+'Modifica contenuto e design &raquo; Impostazioni <b>rilevamento hit</b>',
+
+'jump_systemSettingsWordCensoring' => 
+'Modifica contenuto e design &raquo; Impostazioni di <b>censura delle parole</b>',
+
+'jump_systemSettingsMenuManager' => 
+'Modifica contenuto e design &raquo; Impostazioni <b>gestione menu</b>',
+
+'jump_systemSettingsMembers' => 
+'Modifica impostazioni dei <b>utenti</b>',
+
+'jump_systemSettingsMessages' => 
+'Modifica utenti & raquo; Impostazioni <b>messaggi</b>',
+
+'jump_systemSettingsAvatars' => 
+'Modifica utenti &raquo; Impostazioni <b>avatar</b>',
+
+'jump_systemSettingsSecurityPrivacy' => 
+'Modifica impostazioni di <b>sicurezza e privacy</b>',
+
+'jump_systemSettingsAccessThrottling' => 
+'Modifica sicurezza e privacy &raquo; Impostazioni di <b>limitazione di accesso</b>',
+
+'jump_systemSettingsCaptcha' => 
+'Modifica sicurezza e privacy &raquo; Impostazioni <b>CAPTCHA</b>',
+
+'jump_systemSettingsConsentRequests' => 
+'Modifica sicurezza e privacy &raquo; Impostazioni <b>richiesta consenso</b>',
+
+'jump_systemUtilitiesCommunicate' => 
+'Utilità di sistema &raquo; <b>Comunicazione</b>',
+
+'jump_systemUtilitiesCommunicateSent' => 
+'Utilità di sistema &raquo; Comunicazione &raquo; <b>Inviata</b>',
+
+'jump_systemUtilitiesTranslation' => 
+'Utilità di sistema &raquo; <b>Traduzioni pannello di controllo</b>',
+
+'jump_systemUtilitiesPHPInfo' => 
+'Utilità di sistema &raquo; <b>PHP Info</b>',
+
+'jump_systemUtilitiesExtensions' => 
+'Utilità di sistema &raquo; <b>Estensioni</b>',
+
+'jump_systemUtilitiesDebugTools' => 
+'Utilità di sistema &raquo; <b>Strumenti di debug</b>',
+
+'jump_systemUtilitiesDebugToolsTags' => 
+'Utilità di sistema &raquo; <b>Template tag di debug</b>',
+
+'jump_systemUtilitiesDebugToolsFieldtypes' => 
+'Utilità di sistema &raquo; <b>Debug tipi di campo</b>',
+
+'jump_systemUtilitiesFileConverter' => 
+'Utilità di sistema &raquo; <b>Convertitore di file</b>',
+
+'jump_systemUtilitiesMemberImport' => 
+'Utilità di sistema &raquo; <b>Importazione utente</b>',
+
+'jump_systemUtilitiesMassNotificationExport' => 
+'Utilità di sistema &raquo; <b>Esportazione di notifiche di massa</b>',
+
+'jump_systemUtilitiesBackupUtility' => 
+'Utilità di sistema &raquo; <b>Utility di backup</b>',
+
+'jump_systemUtilitiesSQLManager' => 
+'Utilità di sistema &raquo; <b>Gestore SQL</b>',
+
+'jump_systemUtilitiesQueryForm' => 
+'Utilità di sistema &raquo; <b>Form query SQL</b>',
+
+'jump_systemUtilitiesCacheManager' => 
+'Utilità di sistema &raquo; <b>Gestore cache</b>',
+
+'jump_systemUtilitiesSearchReindex' => 
+'Utilità di sistema &raquo; <b>Reindicizzazione ricerca</b>',
+
+'jump_systemUtilitiesStatistics' => 
+'Utilità di sistema &raquo; <b>Statistiche</b> ',
+
+'jump_systemUtilitiesSearchAndReplace' => 
+'Utilità di sistema &raquo; <b>Cerca e sostituisci</b>',
+
+'jump_switchTheme' => 
+'Cambia <b>tema</b> in <i>[theme]</i>',
+
+'jump_editSite' => 
+'Modicia <b>sito</b> intitolato <i>[site]</i>',
+
+'jump_switchSite' => 
+'Cambia <b>sito</b> in <i>[site]</i>',
+
+'jump_logout' => 
+'Esci',
+
+'jump_profile' => 
+'Il mio profilo',
+
+'jump_menuLink' => 
+'Link menu',
+
+'jump_quickLink' => 
+'Link veloce',
+
+'jump_toggleSidebar' => 
+'Attiva/disattiva la navigazione laterale',
+
+'jump_viewCategories' => 
+'Visualizza <b>categorie</b>',
+
+'jump_viewMemberRoles' => 
+'Visualizza <b>ruoli utente</b>',
+
+'jump_viewMemberFields' => 
+'Visualizza <b>campi utente</b>',
+
+'jump_editMemberField' => 
+'Modifica <b>campo utente</b> intitolato <i>[field]</i>',
+
+'jump_myProfile' => 
+'Il mio profilo',
+
+'jump_clearCaches' => 
+'Eliminare i <b>dati memorizzati nella cache</b> per <i>[type]</i>',
+
+'jump_exportTemplates' => 
+'Esporta template',
+
+''=>''
+);
+
+// End of File
